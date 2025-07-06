e3cec472d47df137065b9d999fd88f5e46457ca5
jiyun <jyiun27@gmail.com>
2025-07-07 00:20:26 +0900

    라운드랩 시작

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
 //index.js
+const adList = new Swiper('.ad_swiper',{
+    slidesPerView:1, //전체개수가 아니라 보이는 개수가 기준
+    spaceBetween:10, //사이 여백
+    autoplay:{delay:3000},
+    loop:true,
+    pagination: {
+        el: '.swiper-pagination',
+        type:'bullets'
+    },
+});
